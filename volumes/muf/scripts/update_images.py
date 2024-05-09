32d3ba266d455798a41c1df56a5bda72dea71fe0
Armando123x <51831060+Armando123x@users.noreply.github.com>
2024-05-09 17:22:49 -0500

    Update update_images.py

--- volumes/muf/scripts/update_images.py
+++ volumes/muf/scripts/update_images.py
@@ -1,7 +1,16 @@
 from muf import * 
 
+try:
+    obj=plot_IRI(version_IRI=2016)
 
-obj=plot_IRI(version_IRI=2016)
+    obj.run()
+except:
 
-obj.run()
+    with open("/scripts/status.txt","w") as file:
 
+        file.write("0")
+else:
+
+    with open("/scripts/status.txt","w") as file:
+
+        file.write("1")
